697d4c3c3efa0d8eb74b1471cda566030c2973e4
sgurung <sgurung@OIT228469>
2020-02-21 11:40:58 -0500

    adding filter

--- src/components/PetsList.jsx
+++ src/components/PetsList.jsx
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import FilterList from "./FilterList";
 import PetCard from "./PetCard";
 import usePets from "../hooks/usePets";
 import { PetItems } from "../files/PetsData";
+import CategoriesFilterCollapse from "./CategoriesFilterCollapse";
 import { Alert } from "@baltimorecounty/dotgov-components";
 
 const PetsList = () => {
+  const [filterItems, setFilterItems] = useState([
+    { type: "species", name: "Cat", checked: true },
+    { type: "species", name: "Dog", checked: true },
+    { type: "species", name: "Other", checked: true },
+    { type: "sex", name: "Female", checked: true },
+    { type: "sex", name: "Male", checked: true },
+    { type: "size", name: "Small", checked: true },
+    { type: "size", name: "Medium", checked: true },
+    { type: "size", name: "Large", checked: true }
+  ]);
+  const handlePetFilterChange = changeEvent => {
+    console.log("filter--check");
+
+  };
   //const [{ hasError, isLoading }] = usePets();
 
   // if (hasError) {
@@ -25,6 +40,29 @@ const PetsList = () => {
         <p>Loading Baltimore County Adoptable Pets...</p>
       ) : ( */}
       <div className="row">
+        <div className="col-md-3 col-xs-12">
+          <CategoriesFilterCollapse
+            header="SPECIES"
+            id="Popular-filter"
+            onChange={handlePetFilterChange}
+            items={filterItems.filter(item=>(item.type==="species"))}
+          />
+          <CategoriesFilterCollapse
+            header="SEX"
+            id="Popular-filter"
+            onChange={handlePetFilterChange}
+            items={filterItems.filter(item=>(item.type==="sex"))}
+          />
+         
+          <CategoriesFilterCollapse
+            header="SIZE"
+            id="Popular-filter"
+            onChange={handlePetFilterChange}
+            items={filterItems.filter(item=>(item.type==="size"))}
+          />
+        </div>
+        <div className="col-md-9 col-xs-12">
+        <div className="row">
         <FilterList
           items={PetItems}
           renderItem={props => (
@@ -33,6 +71,8 @@ const PetsList = () => {
             </div>
           )}
         />
+        </div>
+        </div>
       </div>
       {/* )} */}
     </React.Fragment>
