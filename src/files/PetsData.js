277e2d9cf85fbd9860eabe183bcc8be94e716785
tmccoy529 <40430541+tmccoy529@users.noreply.github.com>
2020-02-28 14:50:12 -0500

    Added additional pets for testing

--- src/files/PetsData.js
+++ src/files/PetsData.js
@@ -114,5 +114,124 @@ export const PetItems = [
         value: "12/25/2019"
       }
     ]
+  },
+  {
+    id: "ACIMP-2019-061113",
+    dateAdoptedRedeemed: "12/25/19",
+    imageUrl:
+      "https://baltimorecountymd.gov/sebin/x/a/AdoptAPetCatTestImage.jpg",
+
+    imageUrlAltText: "Sniffles is a cat",
+    animalName: "Mittens",
+    aboutMe:
+      "My name is Bubbles Jr. and I'm a handsome giant looking for my forever home! It sometimes take me some time to warm up to new people, but once I do I'm a big love bug! I can be a bit strong on the leash, but with some work on my leash manners I should be just fine! If you're looking for a big guy to enter your heart, come check me out today!",
+    accellaId: "ACC-12346",
+    status: "Alive and Kicking",
+    attributes: [
+      {
+        label: "Species",
+        value: "Cat"
+      },
+      {
+        label: "Breed",
+        value: "Pit Bull Terrier"
+      },
+      {
+        label: "Color",
+        value: "White"
+      },
+      {
+        label: "Secondary Color",
+        value: "Brindle"
+      },
+      {
+        label: "Sex",
+        value: "Female"
+      },
+      {
+        label: "Weight",
+        value: "106.0"
+      },
+      {
+        label: "Weight Unit",
+        value: "lb"
+      },
+      {
+        label: "Age",
+        value: "5"
+      },
+      {
+        label: "Age Unit",
+        value: "Years"
+      },
+
+      {
+        label: "Spayed/Neutered",
+        value: "Yes"
+      },
+      {
+        label: "Shelter Arrival Date",
+        value: "12/25/2019"
+      }
+    ]
+  },
+  {
+    id: "ACIMP-2019-061113",
+    dateAdoptedRedeemed: "12/25/19",
+    imageUrl:
+      "https://baltimorecountymd.gov/sebin/d/i/AdoptAPetCat2TestImage.jpg",
+    imageUrlAltText: "Sniffles is a cat",
+    animalName: "Mittens Jr",
+    aboutMe:
+      "My name is Bubbles Jr. and I'm a handsome giant looking for my forever home! It sometimes take me some time to warm up to new people, but once I do I'm a big love bug! I can be a bit strong on the leash, but with some work on my leash manners I should be just fine! If you're looking for a big guy to enter your heart, come check me out today!",
+    accellaId: "ACC-12346",
+    status: "Alive and Kicking",
+    attributes: [
+      {
+        label: "Species",
+        value: "Cat"
+      },
+      {
+        label: "Breed",
+        value: "Pit Bull Terrier"
+      },
+      {
+        label: "Color",
+        value: "White"
+      },
+      {
+        label: "Secondary Color",
+        value: "Brindle"
+      },
+      {
+        label: "Sex",
+        value: "Female"
+      },
+      {
+        label: "Weight",
+        value: "106.0"
+      },
+      {
+        label: "Weight Unit",
+        value: "lb"
+      },
+      {
+        label: "Age",
+        value: "5"
+      },
+      {
+        label: "Age Unit",
+        value: "Years"
+      },
+
+      {
+        label: "Spayed/Neutered",
+        value: "Yes"
+      },
+      {
+        label: "Shelter Arrival Date",
+        value: "12/25/2019"
+      }
+    ]
   }
 ];
