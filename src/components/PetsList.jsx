c1fdcb2ca3b154ad1230b6f04d639253c5e9e90c
tmccoy529 <40430541+tmccoy529@users.noreply.github.com>
2020-03-10 14:25:52 -0400

    Removed pet filtering and added new attributes

--- src/components/PetsList.jsx
+++ src/components/PetsList.jsx
@@ -1,72 +1,17 @@
 import FilterList from "./FilterList";
 import PetCard from "./PetCard";
 import { PetItems } from "../files/PetsData";
-import React, { useState } from "react";
-import CategoriesFilterCollapse from "./CategoriesFilterCollapse";
+import React from "react";
 
 const PetsList = () => {
-  const [filteredPets, setFilteredPets] = useState([]);
-  const [isFiltering, setIsFiltering] = useState(false);
-  const [filterItems, setFilterItems] = useState([
-    { type: "species", name: "Cat", checked: false },
-    { type: "species", name: "Dog", checked: false },
-    { type: "species", name: "Other", checked: false }
-  ]);
-  //TODO: These codes are only for april demo purpose only, once we have our service these code will be removed.
-  const filterServiceList = itemUpdated => {
-    let finalItems = [];
-    const activeFilteredSpeciesTypes = itemUpdated.filter(
-      item => item.checked === true
-    );
-    setIsFiltering(true);
-    let items = [...PetItems];
-
-    for (const item of items) {
-      var attributesItems = item.attributes;
-      for (const filterItems of activeFilteredSpeciesTypes) {
-        let name = filterItems.name.toLocaleLowerCase();
-        if (
-          attributesItems.find(x => x.value.toLocaleLowerCase() === `${name}`)
-        ) {
-          finalItems.push(item);
-        }
-      }
-      setFilteredPets(finalItems);
-    }
-  };
-
-  const handlePetFilterChange = changeEvent => {
-    setIsFiltering(false);
-    const { checked, name } = changeEvent.target;
-    const itemUpdated = filterItems.map(item => {
-      return item.name.toLocaleLowerCase() === name.toLocaleLowerCase()
-        ? { ...item, checked: checked }
-        : item;
-    });
-    setFilterItems(itemUpdated);
-    const checkedCount = itemUpdated.filter(item => item.checked).length;
-    (checkedCount === 0 || checkedCount === itemUpdated.length? false: true)
-      ? filterServiceList(itemUpdated)
-      : setFilteredPets([]);
-  };
-  const hasFilteredResults = !(isFiltering && filteredPets.length === 0);
-
   return (
     <React.Fragment>
       <div className="row">
-        <div className="col-md-3 col-xs-12">
-          <CategoriesFilterCollapse
-            header="SPECIES"
-            id="Popular-filter"
-            onChange={handlePetFilterChange}
-            items={filterItems.filter(item => item.type === "species")}
-          />
-        </div>
         <div className="col-md-9 col-xs-12">
-          {hasFilteredResults ? (
+          {PetItems ? (
             <div className="col">
               <FilterList
-                items={filteredPets.length > 0 ? filteredPets : PetItems}
+                items={PetItems}
                 renderItem={props => (
                   <div key={props.id}>
                     <PetCard {...props} />
