19984de8634f544afe8c16e9d28bbc8b20d92d22
martypowell <jpowel25@gmail.com>
2020-04-08 20:22:12 -0400

    changed to use useQuery

--- src/pages/Pet.jsx
+++ src/pages/Pet.jsx
@@ -1,19 +1,40 @@
-import React from "react";
+import { Config } from "@baltimorecounty/javascript-utilities";
 import PetDetail from "../components/PetDetail";
 import PetSidebar from "../components/PetSidebar";
+import React from "react";
 import usePets from "../hooks/usePets";
+import { useQuery } from "react-query";
 
-const AdoptablePetsDetails = props => {
+const { getValue } = Config;
+
+const fetchPet = (key, { animalId }) =>
+  fetch(`${getValue("apiRoot")}/${animalId}`).then((resp) => resp.json());
+
+const AdoptablePetsDetails = (props) => {
   const { animalId } = props.match.params;
-  const [{ response, isLoading }] = usePets(`/hub/pets/pets/${animalId}`);
+  const { data, error, isLoading } = useQuery("pet", { animalId }, fetchPet);
+
+  if (!data) {
+    return <p>Loading information for {animalId}...</p>;
+  }
+
+  if (error) {
+    return (
+      <p>
+        We could not load information for {animalId}. Please try again in a few
+        minutes.
+      </p>
+    );
+  }
 
   const {
     imageUrl,
     imageUrlAltText,
     aboutMe,
     attributes,
-    animalName
-  } = response;
+    animalName,
+    gender,
+  } = data;
 
   var petsInformationAboutAdoption = window.pets.informationAbout;
 
@@ -33,6 +54,7 @@ const AdoptablePetsDetails = props => {
                   attributes={attributes}
                   animalName={animalName}
                   animalId={animalId}
+                  gender={gender}
                 />
               </div>
             )}
