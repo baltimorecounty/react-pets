b0b9f3b3b7467665bf7dc5f211c26411d5ff921f
martypowell <jpowel25@gmail.com>
2020-04-08 20:44:04 -0400

    use react query to retreive pet

--- src/pages/Pet.jsx
+++ src/pages/Pet.jsx
@@ -1,18 +1,17 @@
-import { Config } from "@baltimorecounty/javascript-utilities";
+import { ReactQueryConfigProvider, useQuery } from "react-query";
+
+import { GetPet } from "../services/ApiService";
 import PetDetail from "../components/PetDetail";
 import PetSidebar from "../components/PetSidebar";
 import React from "react";
-import usePets from "../hooks/usePets";
-import { useQuery } from "react-query";
-
-const { getValue } = Config;
-
-const fetchPet = (key, { animalId }) =>
-  fetch(`${getValue("apiRoot")}/${animalId}`).then((resp) => resp.json());
 
 const AdoptablePetsDetails = (props) => {
   const { animalId } = props.match.params;
-  const { data, error, isLoading } = useQuery("pet", { animalId }, fetchPet);
+  const { data, error, isLoading } = useQuery("pet", { animalId }, GetPet, {
+    refetchOnWindowFocus: false,
+    refetchOnMount: false,
+    cacheTime: 0,
+  });
 
   if (!data) {
     return <p>Loading information for {animalId}...</p>;
@@ -39,38 +38,40 @@ const AdoptablePetsDetails = (props) => {
   var petsInformationAboutAdoption = window.pets.informationAbout;
 
   return (
-    <div className="dg_internal-template">
-      <div className="container">
-        <div className="row">
-          <div className="col-md-8 col-sm-12">
-            {isLoading ? (
-              <p>Loading Adoptable Pets...</p>
-            ) : (
-              <div id="dg_main-content">
-                <PetDetail
-                  imageUrl={imageUrl}
-                  imageUrlAltText={imageUrlAltText}
-                  aboutMe={aboutMe}
-                  attributes={attributes}
-                  animalName={animalName}
-                  animalId={animalId}
-                  gender={gender}
-                />
-              </div>
-            )}
-            {petsInformationAboutAdoption ? (
-              <div>
-                <h3>Information About Adoption</h3>
-                <p>{petsInformationAboutAdoption}</p>
-              </div>
-            ) : null}
-          </div>
-          <div className="col-md-4 col-sm-12">
-            <PetSidebar />
+    <ReactQueryConfigProvider>
+      <div className="dg_internal-template">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-8 col-sm-12">
+              {isLoading ? (
+                <p>Loading Adoptable Pets...</p>
+              ) : (
+                <div id="dg_main-content">
+                  <PetDetail
+                    imageUrl={imageUrl}
+                    imageUrlAltText={imageUrlAltText}
+                    aboutMe={aboutMe}
+                    attributes={attributes}
+                    animalName={animalName}
+                    animalId={animalId}
+                    gender={gender}
+                  />
+                </div>
+              )}
+              {petsInformationAboutAdoption ? (
+                <div>
+                  <h3>Information About Adoption</h3>
+                  <p>{petsInformationAboutAdoption}</p>
+                </div>
+              ) : null}
+            </div>
+            <div className="col-md-4 col-sm-12">
+              <PetSidebar />
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </ReactQueryConfigProvider>
   );
 };
 
