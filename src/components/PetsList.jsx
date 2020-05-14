49d7b208a543e44eeb8fd15f47609e5632c7ac4e
tmccoy529 <40430541+tmccoy529@users.noreply.github.com>
2020-05-14 11:39:40 -0400

    Updated service to use windows object for status

--- src/components/PetsList.jsx
+++ src/components/PetsList.jsx
@@ -4,12 +4,15 @@ import PetCard from "./PetCard";
 import React from "react";
 
 const { getValue } = Config;
+const petStatus = window.pets.petStatus;
 
 const PetsList = ({ filters = [] }) => (
   <FilterList
-    title="Adoptable Pets"
+    title={`${petStatus} Pets`}
     filters={filters}
-    apiEndpoint={`${getValue("apiRoot")}?status=Adoptable&recordsPerPage=1000`}
+    apiEndpoint={`${getValue(
+      "apiRoot"
+    )}?status=${petStatus}&recordsPerPage=1000`}
     renderItem={(props) => <PetCard {...props} />}
   />
 );
