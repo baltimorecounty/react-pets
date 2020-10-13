6be2a13f8b0f527f2159205297056bd74718f570
tmccoy529 <40430541+tmccoy529@users.noreply.github.com>
2020-10-13 08:22:21 -0400

    Updates to pets sidebar

--- src/components/PetSidebar.jsx
+++ src/components/PetSidebar.jsx
@@ -1,55 +1,62 @@
 import React from "react";
 import {
   SideBarSection,
-  SummaryList
+  SummaryList,
 } from "@baltimorecounty/dotgov-components";
 
 const adoptAblePetsSummaryItems = [
   {
     id: 1,
     icon: "fas fa-map-marker-alt",
-    heading: "Main Office",
-    content: "<p> 13800 Manor Road <br /> Baldwin, Maryland 212013</p>"
+    heading: "Animal Services",
+    content: "<p> 13800 Manor Road <br /> Baldwin, Maryland 212013</p>",
   },
 
   {
     id: 2,
     icon: "far fa-clock",
-    heading: "Office Hours:",
-    content: "<p>Monday through Friday<br />8 a.m. to 4:30 p.m.</p>"
+    heading: "Hours:",
+    content: "<p>Monday through Friday<br />8 a.m. to 4:30 p.m.</p>",
   },
   {
     id: 21,
 
     heading: "Adoption Hours:",
     content:
-      "<p>Monday through Friday<br />Noon to 6 p.m.</p><p>Saturday and Sunday<br/> Noon to 5 p.m.</p>"
+      "<p>Monday through Friday<br />Noon to 6 p.m.</p><p>Saturday and Sunday<br/> Noon to 5 p.m.</p>",
   },
   {
     id: 22,
 
-    heading: "Redemption Hours:",
+    heading: "Redemption And Surrender Hours:",
     content:
-      "<p>Monday through Friday<br />8 a.m. to 6 p.m.</p><p>Saturday and Sunday<br/> 8 a.m. to 5 p.m.</p>"
+      "<p> By Appointment Only</p><p>Monday through Friday<br />8 a.m. to 6 p.m.</p><p>Saturday and Sunday<br/> 8 a.m. to 5 p.m.</p>",
   },
   {
     id: 3,
     icon: "far fa-envelope",
     heading: "Email:",
-    content: "<p><a href=`#`>animalservices@baltimorecountymd.gov</a></p>"
+    content: "<p><a href=`#`>animalservices@baltimorecountymd.gov</a></p>",
   },
   {
     id: 4,
     icon: "fas fa-mobile-alt",
     heading: "Phone:",
-    content: "<p>410-887-PAWS (phone)</p> <p>410-817-4257 (fax)</p>"
+    content: "<p>410-887-PAWS (phone)</p> <p>410-817-4257 (fax)</p>",
+  },
+  {
+    id: 5,
+    icon: "fas fa-exclamation-circle",
+    heading: "Baltcogo311:",
+    content:
+      "<p><a href='https://www.baltimorecountymd.gov/report-problem/'/>Report a problem</a></p>",
   },
   {
     id: 6,
     icon: "fas fa-user",
     heading: "Chief:",
-    content: "<p>Kevin Usilton</p>"
-  }
+    content: "<p>Kevin Usilton</p>",
+  },
 ];
 
 const followUsSummaryItems = [
@@ -58,8 +65,8 @@ const followUsSummaryItems = [
     icon: "dg_icon fab fa-facebook-square",
     heading: "Animal Services",
     content:
-      "<p>follow <a href=https://www.facebook.com/BaltCoAnimalServices/</>@BaltCoAnimalServices</p>"
-  }
+      "<p>follow <a href=https://www.facebook.com/BaltCoAnimalServices/</>@BaltCoAnimalServices</p>",
+  },
 ];
 
 const PetSidebar = () => {
